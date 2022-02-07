dd790acb9ff2085adc22fae7166e47c0beb4600b
arleen <arleenpandher11@gmail.com>
2022-02-07 15:25:14 -0500

    activity backend

---
+++ routes/api/activities.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const Activity = require('../../models/Activity')
+const router = express.Router()
+
+
+router.get("/", (req, res) => {
+    Activity.find()
+    .then(activities => res.json(activities))
+    .catch(err => res.status(400).json(err))
+})
+
+router.get("/:id", (req, res) => {
+    Activity.findById(req.params.id)
+    .then(activity => res.json(activity))
+    .catch(err => res.status(400).json(err))
+})
+
+module.exports = router;
