20fcbbd0b3aa23f64f236765b66fdc548923c127
Kirti Harode <kikihrd20@gmail.com>
2022-02-08 17:19:44 -0800

    cryd for calendar

---
+++ frontend/src/actions/calendar_action.js
@@ -0,0 +1,38 @@
+import * as CalendarApiUtil from '../util/calendar_api_util';
+
+export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
+export const RECEIVE_USER_EVENTS = "RECEIVE_USER_EVENTS";
+export const RECEIVE_NEW_EVENT = "RECEIVE_NEW_EVENT";
+
+export const receiveEvents = events => ({
+  type: RECEIVE_EVENTS,
+  events
+});
+
+export const receiveUserEvents = events => ({
+  type: RECEIVE_USER_EVENTS,
+  events
+});
+
+export const receiveNewEvent = event => ({
+  type: RECEIVE_NEW_EVENT,
+  event
+})
+
+export const fetchEvent = () => dispatch => (
+    CalendarApiUtil.getEvents()
+    .then(events => dispatch(receiveEvents(events)))
+    .catch(err => console.log(err))
+);
+
+export const getUserEvents = id => dispatch => (
+    CalendarApiUtil.getUserEvents(id)
+    .then(events => dispatch(receiveUserEvents(events)))
+    .catch(err => console.log(err))
+);
+
+export const createEvent = data => dispatch => (
+    CalendarApiUtil.createEvent(data)
+    .then(event => dispatch(receiveNewEvent(event)))
+    .catch(err => console.log(err))
+);
