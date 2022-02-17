5c01288cbbc27919a4ae93180cb2d96746d28142
Kirti Harode <kikihrd20@gmail.com>
2022-02-16 16:59:05 -0800

    map is working

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -4,62 +4,62 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX
 
-const data = [
-	{
-		"location": "Animal Shelter, 1200 15th St",
-		"city": "San Francisco",
-		"state": "California",
-		"coordinates": [-122.415186, 37.7669592],
-	},
-	{
-		"location": "San Francisco SPCA Veterinary Hospital - Pacific Heights Campus, 2343 Fillmore Street",
-		"city": " San Francisco",
-		"state": "California",
-		"coordinates": [-122.436753, 37.7913051],
-	},
-	{
-		"location": "Pets unlimited, 675 Georgia Ave",
-		"city": " Sunnyvale",
-		"state": "California",
-		"coordinates": [-122.0206964, 37.3918082],
-	},
-    {
-		"location": "Peninsula Humane Society & SPCA Adoption Center, 1450 Rollins Rd",
-		"city": "Burlingame",
-		"state": "California",
-		"coordinates": [-122.3669955, 37.589781],
-	},
-    {
-		"location": "San Francisco Animal Care & Control, 1419 Bryant St",
-		"city": " San Francisco",
-		"state": "California",
-		"coordinates": [-122.4123976, 37.7685775],
-	},
-    {
-		"location": "San Francisco SPCA Mission Adoption Center, 250 Florida St",
-		"city": " San Francisco",
-		"state": "California",
-		"coordinates": [-122.4139514, 37.7662966],
-	},
-    {
-		"location": "Palo Alto Humane Society, 4000 Middlefield Rd D1",
-		"city": "Palo Alto",
-		"state": "California",
-		"coordinates": [-122.1107855, 37.4168785],
-	},
-    {
-		"location": "Friends of the Alameda Animal Shelter (FAAS) South Shore, 2228 S Shore Center",
-		"city": "Alameda",
-		"state": "California",
-		"coordinates": [-122.253695, 37.7569359],
-	},
-    {
-		"location": "Nine Lives Foundation Adoption Center, 3106 Rolison Rd ",
-		"city": "Redwood City",
-		"state": "California",
-		"coordinates": [-122.1999119, 37.4861079],
-	}
-]
+// const data = [
+// 	{
+// 		"location": "Animal Shelter, 1200 15th St",
+// 		"city": "San Francisco",
+// 		"state": "California",
+// 		"coordinates": [-122.415186, 37.7669592],
+// 	},
+// 	{
+// 		"location": "San Francisco SPCA Veterinary Hospital - Pacific Heights Campus, 2343 Fillmore Street",
+// 		"city": " San Francisco",
+// 		"state": "California",
+// 		"coordinates": [-122.436753, 37.7913051],
+// 	},
+// 	{
+// 		"location": "Pets unlimited, 675 Georgia Ave",
+// 		"city": " Sunnyvale",
+// 		"state": "California",
+// 		"coordinates": [-122.0206964, 37.3918082],
+// 	},
+//     {
+// 		"location": "Peninsula Humane Society & SPCA Adoption Center, 1450 Rollins Rd",
+// 		"city": "Burlingame",
+// 		"state": "California",
+// 		"coordinates": [-122.3669955, 37.589781],
+// 	},
+//     {
+// 		"location": "San Francisco Animal Care & Control, 1419 Bryant St",
+// 		"city": " San Francisco",
+// 		"state": "California",
+// 		"coordinates": [-122.4123976, 37.7685775],
+// 	},
+//     {
+// 		"location": "San Francisco SPCA Mission Adoption Center, 250 Florida St",
+// 		"city": " San Francisco",
+// 		"state": "California",
+// 		"coordinates": [-122.4139514, 37.7662966],
+// 	},
+//     {
+// 		"location": "Palo Alto Humane Society, 4000 Middlefield Rd D1",
+// 		"city": "Palo Alto",
+// 		"state": "California",
+// 		"coordinates": [-122.1107855, 37.4168785],
+// 	},
+//     {
+// 		"location": "Friends of the Alameda Animal Shelter (FAAS) South Shore, 2228 S Shore Center",
+// 		"city": "Alameda",
+// 		"state": "California",
+// 		"coordinates": [-122.253695, 37.7569359],
+// 	},
+//     {
+// 		"location": "Nine Lives Foundation Adoption Center, 3106 Rolison Rd ",
+// 		"city": "Redwood City",
+// 		"state": "California",
+// 		"coordinates": [-122.1999119, 37.4861079],
+// 	}
+// ]
 
 class MapComponent extends React.Component{
 	constructor(props){
@@ -93,7 +93,7 @@ class MapComponent extends React.Component{
             });
         });
 		// console.log("this is map_data" + this.props.data)
-		data.forEach((location) => {
+		this.props.data.forEach((location) => {
 			var marker = new mapboxgl.Marker()
                         .setLngLat(location.coordinates)
                         .setPopup(new mapboxgl.Popup({ offset: 30 })
